2e5b1f7f7e3e76797f86eb6a2ea6dbf6aeb326bb
Ederjoel26 <eder_1321124031@uptecamac.edu.mx>
2022-12-01 12:32:28 -0600

    Trying some things

--- src/routes/user.js
+++ src/routes/user.js
@@ -22,7 +22,7 @@ const storage = multer.diskStorage({
         cb(null, 'src/uploads');
     },
     filename: (req, file, cb) => {
-        cb(null, file.originalname );   
+        cb(null, file.originalname + Date.now());   
     }
 });
 
